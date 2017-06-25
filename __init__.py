75426f62395af0c0b02101c3604632720a81331f
Ravi Kumar Seth <ravi.kseth.che12@iitbhu.ac.in>
2017-06-26 02:35:04 +0530

    Update path of client_secret

--- __init__.py
+++ __init__.py
@@ -59,7 +59,7 @@ def gconnect():
 
         # Upgrade the authorization code into a credentials object
 
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow = flow_from_clientsecrets(path + '/client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
