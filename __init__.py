5deedb058b9482edf6eda21b25c3168564781be2
Ravi Kumar Seth <ravi.kseth.che12@iitbhu.ac.in>
2017-06-28 19:45:29 +0530

    edit: Typo Correction

--- __init__.py
+++ __init__.py
@@ -150,7 +150,7 @@ def gconnect():
 # User Helper Functions
 
 def createUser(login_session):
-    newUser = User(name=login_session['username'],
+    newUser = Users(name=login_session['username'],
                    email=login_session['email'],
                    picture=login_session['picture'])
     session.add(newUser)
